88147f256095d93e254e9cef30b75a68bc5ce46b
Bohdon Sayre <bohdon@gmail.com>
2017-10-20 19:56:06 -0500

    adds sublime prefs installation to win setup script

--- setup_win.sh
+++ setup_win.sh
@@ -1,10 +1,13 @@
 set -x
 
 # code
-cp -a "prefs/Code/win/." "$HOME/AppData/Roaming/Code/"
+cp -av "prefs/Code/win/." "$HOME/AppData/Roaming/Code/"
 
 # conemu
-cp -a "prefs/ConEmu/ConEmu.xml" "$HOME/AppData/Roaming/"
+cp -av "prefs/ConEmu/ConEmu.xml" "$HOME/AppData/Roaming/"
+
+# sublime text
+cp -av "prefs/Sublime Text 3/." "$HOME/AppData/Roaming/Sublime Text 3/"
 
 # git
 git config --global include.path "~/dev/workspace/prefs/git/gitconfig"
