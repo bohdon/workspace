157c819df4ae283e0368837547c4a63addc884b8
Bohdon Sayre <bohdon@gmail.com>
2017-11-21 01:29:27 -0600

    adds code and sublime text setup for mac

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,4 +1,10 @@
 set -x
 
+# code
+cp -av "prefs/Code/mac/." "$HOME/Library/Application Support/Code/"
+
+# sublime text
+cp -av "prefs/Sublime Text 3/." "$HOME/Library/Application Support/Sublime Text 3/"
+
 # git
 git config --global include.path "~/dev/workspace/prefs/git/gitconfig"
