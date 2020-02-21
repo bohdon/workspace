88efc82283fefa3a781ebab0b0f61957e856c68b
Bohdon Sayre <bohdon@gmail.com>
2020-02-21 14:54:00 -0500

    add install script for caps lock as control

--- install_win.sh
+++ install_win.sh
@@ -5,7 +5,12 @@ fi
 
 # Explorer Settings
 if should_install "Explorer prefs"; then
-    powershell -ExecutionPolicy Bypass -File "prefs\Explorer\SetExplorerSettings.ps1"
+    powershell -ExecutionPolicy Bypass -File "prefs/Explorer/SetExplorerSettings.ps1"
+fi
+
+# Caps Loc to Control
+if should_install "Caps Lock as Control"; then
+    regedit.exe "prefs/Windows/CapsLockMappedToControl.reg"
 fi
 
 # Visual Studio Code
