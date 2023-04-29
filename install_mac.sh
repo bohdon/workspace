a1e946e6bcc41d4289af7430a41995f0d4941fe8
Bohdon Sayre <bohdon@gmail.com>
2023-04-29 02:53:19 -0400

    remove old prefs

--- install_mac.sh
+++ install_mac.sh
@@ -8,11 +8,6 @@ if should_install "Visual Studio Code prefs"; then
     cp -av "prefs/Code/mac/." "$HOME/Library/Application Support/Code/"
 fi
 
-# Sublime Text 3
-if should_install "Sublime Text 3 prefs"; then
-    cp -av "prefs/Sublime Text 3/." "$HOME/Library/Application Support/Sublime Text 3/"
-fi
-
 # Git Config
 if should_install "Git Config"; then
     set -x
