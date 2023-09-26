676813380e4768d6481fa1bdc960a04a9c349ecf
Bohdon Sayre <bohdon@gmail.com>
2023-09-26 16:46:43 -0400

    add settings for disabling one drive in quick access, and enabling full right click menu in windows 11

--- prefs/Explorer/SetExplorerSettings.ps1
+++ prefs/Explorer/SetExplorerSettings.ps1
@@ -6,9 +6,20 @@ Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer -Name ShowRecent -Type DWord -Value 0 # default 1
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer -Name ShowFrequent -Type DWord -Value 0 # default 1
 
+# Disable Quick Access: One Drive
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Classes\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Name System.IsPinnedToNameSpaceTree -Type DWord -Value 0 # default 1
+
 # Change Explorer to show hidden files and file extensions
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name Hidden -Type DWord -Value 1 # default 2
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name HideFileExt -Type DWord -Value 0 # default 1
 
 # Disable shake-to-minimze
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name DisallowShaking -Type DWord -Value 1
+
+# Enable full right click menu in Windows 11
+reg add "HKCU\Software\Classes\CLSID\{86CA1AA0-34AA-4E8B-A509-50C905BAE2A2}\InprocServer32" /f /ve
+
+
+# restart explorer
+taskkill /F /IM explorer.exe
+start explorer
