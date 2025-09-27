47089d2d796bc2166d4483ea09dcc2727bf7c46a
Bohdon Sayre <bohdon@gmail.com>
2025-09-27 18:59:05 -0400

    remove Home from side bar in explorer settings, fix some logging

--- prefs/Explorer/SetExplorerSettings.ps1
+++ prefs/Explorer/SetExplorerSettings.ps1
@@ -6,8 +6,13 @@ Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer -Name ShowRecent -Type DWord -Value 0 # default 1
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer -Name ShowFrequent -Type DWord -Value 0 # default 1
 
+# Remove Home from side bar
+REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{e88865ea-0e1c-4e20-9aa6-edcd0212c87c}" /f *> $null
+REG DELETE "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{f874310e-b6b7-47dc-bc84-b9e6b38f5903}" /f *> $null
+REG ADD "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /f /v "LaunchTo" /t REG_DWORD /d "1" > $null
+
 # Disable Quick Access: One Drive
-Set-ItemProperty -Path "HKCU:\SOFTWARE\Classes\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Name System.IsPinnedToNameSpaceTree -Type DWord -Value 0 # default 1
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Classes\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Name System.IsPinnedToNameSpaceTree -Type DWord -Value 0 2> $null
 
 # Change Explorer to show hidden files and file extensions
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name Hidden -Type DWord -Value 1 # default 2
@@ -17,7 +22,7 @@ Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name DisallowShaking -Type DWord -Value 1
 
 # Enable full right click menu in Windows 11
-reg add "HKCU\Software\Classes\CLSID\{86CA1AA0-34AA-4E8B-A509-50C905BAE2A2}\InprocServer32" /f /ve
+New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Name "InprocServer32" -force -value "" > $null
 
 # Disable start menu online search
 if (-not (Test-Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer")) {
@@ -26,5 +31,5 @@ if (-not (Test-Path "HKCU:\Software\Policies\Microsoft\Windows\Explorer")) {
 Set-ItemProperty -Path HKCU:\Software\Policies\Microsoft\Windows\Explorer -Name DisableSearchBoxSuggestions -Type DWord -Value 1 # default 0 / non-existent
 
 # restart explorer
-taskkill /F /IM explorer.exe
+taskkill /F /IM explorer.exe > $null
 start explorer
